b57f9aaa41132b64fdd771faa753a668012d4c7b
paledust <paledust@hotmail.com>
2024-09-28 18:12:58 +0800

    update several new features

--- Assets/DevelopBasic/Util/Extension.cs
+++ Assets/DevelopBasic/Util/Extension.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 #region Extension
 public static class ExtensionMethods{
-    /// <summary>
-    /// Play a random audio clip from a series of audio clips
-    /// </summary>
-    /// <param name="audio">Target audio source</param>
-    /// <param name="clips">Clips to be played</param>
-    /// <param name="volume">How loud the clip should be played</param>
-    /// <param name="pitchShiftRange">How much can the pitch be shifted</param>
-    public static void PlayRandomClipFromClips(this AudioSource audio, AudioClip[] clips, float volume =1, float pitchShiftRange = 0){
-        audio.pitch = 1+Random.Range(-pitchShiftRange, pitchShiftRange);
-        audio.PlayOneShot(clips[Random.Range(0, clips.Length)], volume);
+    public static Vector3 GetPositionOnPlaneFromScreenPoint(this Camera camera, Vector3 screenPoint, Vector3 planePoint, Vector3 planeNormal){
+        Ray ray = camera.ScreenPointToRay(screenPoint);
+
+        if(Vector3.Dot(ray.direction, planeNormal) == 0) return Vector3.zero;
+
+        Vector3 offset = planePoint - ray.origin;
+        float height = Vector3.Dot(offset, planeNormal);
+        float scale  = height/Vector3.Dot(ray.direction, planeNormal);
+        Vector3 planeOffset = ray.direction*scale - offset;
+        
+        return planeOffset + planePoint;
     }
     public static float GetRndValueInVector2Range(this Vector2 range){return Random.Range(range.x, range.y);}
     public static int GetRndValueInVector2Range(this Vector2Int range){return Random.Range(range.x, range.y);}
